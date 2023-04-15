ab2c76108ae9526a25ff6637e00fabcfb9221c5b
Gabriel Alves <37886654+GabrielDragone@users.noreply.github.com>
2023-04-15 18:37:36 -0300

    Criação do projeto e finalização do tópico inicial

---
+++ src/test/java/br/com/gabrieldragone/mspagamentos/MsPagamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrieldragone.mspagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPagamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
